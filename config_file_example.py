c12cec74100e58182725c0d9b30564aed9ac69a5
Externo.1 <externo.1@logikers.com>
2023-04-17 15:02:27 +0200

    Added README.md info

---
+++ config_file_example.py
@@ -0,0 +1,13 @@
+# Rename this file to config_file.py for using it
+
+# Temporal path to store the images and frames extracted from videos
+TMP_PATH = 'd:/tmp/images/'
+
+
+# Files to remove are included in to_remove.py with this format:
+# Each time the script detects any of the string included, the file
+# is filtered out.
+
+to_remove = ['folder string', \
+             'folder string 2', \
+             'folder string 3']
